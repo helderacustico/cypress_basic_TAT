4a7639398e3f33860e639ddf8eb887afd94d5fc1
helderacustico <helderacustico@hotmail.com>
2022-12-22 22:20:07 -0300

    continuação das aulas

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -12,15 +12,93 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        
+        const longText = 'Teste de descrição, Teste de descrição, Teste de descrição, Teste de descrição, Teste de descrição, Teste de descrição, Teste de descrição, Teste de descrição'
 
         cy.get('#firstName').type('Helder')
         cy.get('#lastName').type('Lima Soares')
         cy.get('#email').type('helder@teste.com.br')
-        cy.get('#open-text-area').type('Teste de descrição, Teste de descrição')
-        cy.get('button[type="submit"]').click()
+        cy.get('#open-text-area').type(longText, { delay: 0 })
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.success').should('be.visible')
     })
+
+    it('exibe mensagem de erro ao submeter o formulário com um e-mail com formatação inválida', function() {
+        
+        cy.get('#firstName').type('Helder')
+        cy.get('#lastName').type('Lima Soares')
+        cy.get('#email').type('helder@teste,com')
+        cy.get('#open-text-area').type('Teste')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+
+        
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
+        
+        cy.get('#phone')
+            .type('abcdefghij')
+            .should('have.value', '')
+
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido', function(){
+
+        cy.get('#firstName').type('Helder')
+        cy.get('#lastName').type('Lima Soares')
+        cy.get('#email').type('helder@teste.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Teste')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+
+        cy.get('#firstName')
+            .type('Helder')
+            .should('have.value', 'Helder')
+            .clear()
+            .should('have.value', '')
+        
+        cy.get('#lastName')
+            .type('Lima Soares')
+            .should('have.value', 'Lima Soares')
+            .clear()
+            .should('have.value', '')
+
+        cy.get('#email')
+            .type('helder@teste.com')
+            .should('have.value', 'helder@teste.com')
+            .clear()
+            .should('have.value', '')
+        
+        cy.get('#open-text-area')
+            .type('77991364331')
+            .should('have.value', '77991364331')
+            .clear()
+            .should('have.value', '')
+
+
+    })
+
+    it('exibe mensagem de erro ao submeter o furmulário sem preencher os campos obrigatórios', function() {
+        
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formulário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSumbmit()
+
+        cy.get('.success').should('be.visible')
+    })    
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,9 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSumbmit', function() {
+    
+    cy.get('#firstName').type('Helder')
+    cy.get('#lastName').type('Lima Soares')
+    cy.get('#email').type('helder@teste.com.br')
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button', 'Enviar').click()
+
+})
